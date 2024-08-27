5743e82b335a2c6d8b6a57a242e6c98eda5f1a45
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-27 19:08:59 +0300

    Step2 login

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Bloglist app', () => {
   beforeEach(async ({ page, request }) => {
@@ -16,8 +17,23 @@ describe('Bloglist app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('Bloglist')).toBeVisible()
     await expect(page.getByText('Log in to application')).toBeVisible()
-    await expect(page.getByTestId('username')).toBeVisible()
-    await expect(page.getByTestId('password')).toBeVisible()
-    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'holmes', 'baskerville')
+      const userDiv = await page.locator('.user')
+      await expect(userDiv).toContainText('Sherlock Holmes logged in')
+      await expect(userDiv).toHaveCSS('color', 'rgb(143, 130, 46)')
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'holmes', 'basker')
+      await expect(page.getByText('Sherlock Holmes logged in')).not.toBeVisible()
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('ERROR: Wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(143, 46, 46)')
+    })
   })
 })
